d26a7ad809ff96d3da3e4a701503e225a42f288c
Sushruta18 <sushruta.rapolu@gmail.com>
2026-03-25 22:45:01 +0530

    Fix IPv6 CORS issue

--- main.py
+++ main.py
@@ -26,11 +26,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,   # IMPORTANT
     allow_methods=["*"],
     allow_headers=["*"],
 )
